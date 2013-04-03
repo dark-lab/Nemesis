ca9d950741d3d34647f5067afc526070614c7353
mudler <mudler@dark-lab.net>
2013-04-03 20:00:38 +0200

    small changes

--- Plugin/Scanner.pm
+++ Plugin/Scanner.pm
@@ -7,18 +7,70 @@ class Plugin::Scanner {
     our $MODULE  = "Scanner plugin";
     our $INFO    = "<www.dark-lab.net>";
 
-    our @PUBLIC_FUNCTIONS = qw(info test );
+    our @PUBLIC_FUNCTIONS = qw(info test nmap);
 
     nemesis_moosex_module;
-
-
-    method test($String) {
+    use HTTP::Request;
+    use Net::IP;
+    use Nmap::Parser;
+    method test($SearchString,$Exploit) {
         my $Crawler=$self->Init->getModuleLoader()->loadmodule("Crawler");
-        $Crawler->search($String);
+        $Crawler->search($SearchString);
         $Crawler->fetchNext();
+        my $LFI=$self->Init->getModuleLoader()->loadmodule("LFI");
+        $LFI->Bug($Exploit);#Can be post or otherwise, so should implement the api with HTTP::Request object.
+        $LFI->Crawler($Crawler);
+        $LFI->test();
+   }
+   method nmap($Ip?){
+
+    if($Ip) {
+
+
+    } else {
+        my @Ips = $self->Init->getInterfaces()->ips();
+        foreach my $Ip(@Ips){
+                        my $NIP=Net::IP->new($Ip);
+
+            $self->Init->getIO()->print_info("IP: ".$Ip."/24");
+            $self->nmapscan($Ip);
+
+
+        }
+    }
+
+
 
 
    }
+
+   method nmapscan($Ip){
+    my $Np=Nmap::Parser->new();
+    $Np->parsescan($self->Init->getEnv()->whereis("nmap"), "-sS -sV -O -A -P0 $Ip");
+        my $Session=$Np->get_session;
+
+        $self->Init->getIO()->print_info("Session:".$Session->scan_args);
+
+    foreach my $host($Np->all_hosts()){
+
+        my $os         = $host->os_sig();
+
+        $self->Init->getIO()->print_info($host->addr);
+        $self->Init->getIO()->print_tabbed("Status: ".$host->status,3);
+        $self->Init->getIO()->print_tabbed("HostNames: ".join(" ",$host->all_hostnames()),3);
+        $self->Init->getIO()->print_tabbed("Mac HW: ".$host->mac_addr(),3);
+
+        for my $port ($host->tcp_ports()){
+            my $service = $host->tcp_service($port);
+            my $os = $host->os_sig;
+            $self->Init->getIO()->print_info($host->hostname().",".$host->ipv4_addr().",".$host->mac_addr().",".$os->name.",".$os->family.",".$os->osgen().",".$os->name_accuracy().",".$port.",".$service->name.",".$service->product.",".$service->version.",".$service->confidence());
+        }
+
+    }
+
+   }
+
+
 
 }
 1;
