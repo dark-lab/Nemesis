6189385c0c0d7fe329190257ec97dbefb96a4309
mudler <mudler@dark-lab.net>
2013-04-06 19:46:10 +0200

    Fixed align

--- Nemesis/Inject.pm
+++ Nemesis/Inject.pm
@@ -7,93 +7,31 @@ sub import {
     # create keyword 'provided', expand it to 'if' at parse time
     Keyword::Simple::define 'nemesis_module', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-         our $Init;
-         sub new(){
-			my $package = shift;
-			bless( {}, $package );
-			%{ $package } = @_;
-			$Init=$package->{\'Init\'};
-			return $package;
-         }
-         sub export_public_methods() {   
-		    my $self = shift;
-		
-		    return @PUBLIC_FUNCTIONS;
-		}
-		sub info(){
-		$Init->getIO()->print_tabbed("__PACKAGE__ $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);
-		}
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = ' our $Init;sub new(){my $package = shift;bless( {}, $package );%{ $package } = @_;$Init=$package->{\'Init\'};return $package;}sub export_public_methods() {   my $self = shift;return @PUBLIC_FUNCTIONS;}sub info(){$Init->getIO()->print_tabbed("__PACKAGE__ $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);	} ';    # inject 'if' at beginning of parse buffer
     };
 
     Keyword::Simple::define 'nemesis_moose_module', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-		has \'Init\' => (
-			is=>\'rw\',
-			required=> 1
-			);
-		sub export_public_methods() {   
-		    return @PUBLIC_FUNCTIONS;
-			}
-		sub info(){
-			my $self=shift;
-			$self->Init->getIO()->print_tabbed(__PACKAGE__ ." $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);
-		}
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = '		has \'Init\' => (			is=>\'rw\',			required=> 1			);		sub export_public_methods() {		    return @PUBLIC_FUNCTIONS;			}		sub info(){			my $self=shift;			$self->Init->getIO()->print_tabbed(__PACKAGE__ ." $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);		}         ';    # inject 'if' at beginning of parse buffer
     };
 
     Keyword::Simple::define 'nemesis_moosex_module', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-		has Init => (
-			is=> \'rw\',
-			required=> 1
-			);
-		method export_public_methods() {   
-		    return @PUBLIC_FUNCTIONS;
-			}
-		method info(){
-			$self->Init->getIO()->print_tabbed(__PACKAGE__ ." $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);
-		}
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = '		has Init => (			is=> \'rw\',			required=> 1			);		method export_public_methods() {   		    return @PUBLIC_FUNCTIONS;			}		method info(){			$self->Init->getIO()->print_tabbed(__PACKAGE__ ." $MODULE v$VERSION ~ $AUTHOR ~ $INFO",2);		}         ';    # inject 'if' at beginning of parse buffer
     };
 
     Keyword::Simple::define 'nemesis_moosex_resource', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-		has Init => (
-			is=> \'rw\',
-			required=> 1
-			);
-
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = '		has Init => (			is=> \'rw\',			required=> 1			);         ';    # inject 'if' at beginning of parse buffer
     };
 
     Keyword::Simple::define 'nemesis_moose_resource', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-		has \'Init\' => (
-			is=> read_write,
-			required=> true
-			);
-
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = '		has \'Init\' => (			is=> read_write,			required=> true			);         ';    # inject 'if' at beginning of parse buffer
     };
     Keyword::Simple::define 'nemesis_resource', sub {
         my ($ref) = @_;
-        substr( $$ref, 0, 0 ) = '
-  our $Init;
-         sub new(){
-			my $package = shift;
-			bless( {}, $package );
-			%{ $package } = @_;
-			$Init=$package->{\'Init\'};
-			return $package;
-         }
-       
-         ';    # inject 'if' at beginning of parse buffer
+        substr( $$ref, 0, 0 ) = '  our $Init;         sub new(){			my $package = shift;			bless( {}, $package );			%{ $package } = @_;			$Init=$package->{\'Init\'};			return $package;         }                ';    # inject 'if' at beginning of parse buffer
     };
 
 }
