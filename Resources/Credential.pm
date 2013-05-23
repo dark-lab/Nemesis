e01550489ac00f4a0674afdf446449eee11b6f7c
luca9010 <luca9010@hotmail.it>
2013-05-23 13:49:43 +0200

    fix field

--- Resources/Credential.pm
+++ Resources/Credential.pm
@@ -4,7 +4,8 @@ class Resources::Credential{
 	
 
 	has 'SITE' => (is=>"rw"); # site where this credential has been captured
-	has 'PARAMAMETER' => (is=>"rw"); # parameter of the credential (maybe 'password')
+	has 'PARAMETER' => (is=>"rw"); # parameter of the credential (maybe 'password')
 	has 'VALUE' => (is=>'rw'); # value of the credential (maybe the password sniffed)
+	#has 'CREDENTIAL' => (is=>"rw", isa=>"ArrayRef", default => sub { [] }); # array of credential (parameter->value)
 
 }
