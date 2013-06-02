58a0475b8dff3af6856c421614d5fc26f1c47eea
mudler <mudler@dark-lab.net>
2013-06-02 22:34:15 +0200

    fixes for moose integration

--- Resources/Dispatcher.pm
+++ Resources/Dispatcher.pm
@@ -1,39 +1,45 @@
-use MooseX::Declare;
+
+package Resources::Dispatcher;
+use Moose;
 use Nemesis::Inject;
 
-class Resources::Dispatcher{
+nemesis_resource;
 
-		nemesis_resource;
+sub dispatch_packet() {
+    my $self  = shift;
+    my $Frame = shift;
 
-		method dispatch_packet($Frame){
+    #$Init->io->info($Frame->print);
+    #print "LAyer: ".join(",",$oSimple->layers)."\n";
 
-		    #$Init->io->info($Frame->print);
-     	  #print "LAyer: ".join(",",$oSimple->layers)."\n";
+    # my $this=shift @Packet_info;
+    #my $npe=shift @Packet_info;
+    # $self->debug($po);
+    #$Init->io->debug("my Frame is ".$Frame);
+    foreach my $data ( $Frame->layers ) {
+        my ($Type) = $data =~ /.*\:\:(.*?)\=/;
 
-   
-		   # my $this=shift @Packet_info;
-		    #my $npe=shift @Packet_info;
-		   # $self->debug($po);
-			#$Init->io->debug("my Frame is ".$Frame);
-			foreach my $data($Frame->layers){
-			my ($Type) = $data=~/.*\:\:(.*?)\=/;
-			#$Init->io->debug("$data");
-				if(defined($Type)){
-					#$Init->getIO->debug("$data is $Type");
-					$self->match("event_".lc($Type),$Frame);
-					#$self->debug($data);
-				}
-			}
-		}
+        #$Init->io->debug("$data");
+        if ( defined($Type) ) {
 
-		method match(@Args){
-			my $Command=shift(@Args);
-			foreach my $Module($self->Init->getModuleLoader->canModule($Command)){	
-		    	my $Instance=$self->Init->getModuleLoader->getInstance($Module);
-		    	#$self->Init->getIO()->print_info("I can do that $Instance");
-		    	eval { $Instance->$Command(@Args); };
-		    }
-		}
+            #$Init->getIO->debug("$data is $Type");
+            $self->match( "event_" . lc($Type), $Frame );
 
-
+            #$self->debug($data);
+        }
+    }
 }
+
+sub match() {
+    my $self    = shift;
+    my @Args    = @_;
+    my $Command = shift(@Args);
+    foreach my $Module ( $self->Init->getModuleLoader->canModule($Command) ) {
+        my $Instance = $self->Init->getModuleLoader->getInstance($Module);
+
+        #$self->Init->getIO()->print_info("I can do that $Instance");
+        eval { $Instance->$Command(@Args); };
+    }
+}
+1;
+
--- Resources/Monitor.pm
+++ Resources/Monitor.pm
@@ -1,66 +1,71 @@
-use MooseX::Declare;
+package Resources::Monitor;
+
+use Moose;
 
 use Nemesis::Inject;
-class Resources::Monitor{
-use Resources::Dispatcher;
 use Net::Frame::Simple;
 use Net::Frame::Dump::Online;
 
-	has 'Device' => (is=>"rw",default=>"wlan0");
-	has 'Filter' => (is=>"rw",default=>"");
-	has 'Promiscuous' => (is=>"rw",default=>"1");
-	has 'Dispatcher' => (is=>"rw");
-	has 'File'		 => (is=>"rw", default=>undef);
-	has 'SnifferInstance' => (is=>"rw");
-	has 'Dispatcher'	 => (is=>"rw");
-	nemesis_resource;
+has 'Device'      => ( is => "rw", default => "wlan0" );
+has 'Filter'      => ( is => "rw", default => "" );
+has 'Promiscuous' => ( is => "rw", default => "1" );
+has 'Dispatcher'  => ( is => "rw" );
+has 'File'        => ( is => "rw", default => undef );
+has 'SnifferInstance' => ( is => "rw" );
+has 'Dispatcher'      => ( is => "rw" );
+nemesis_resource;
 
+$SIG{'TERM'} = sub { exit; };
 
-	$SIG{'TERM'}=sub { exit; };
+sub run() {
+    my $self = shift;
+    my $d    = $self->Init->getModuleLoader->loadmodule("Dispatcher");
+    $self->Dispatcher($d);
+    $self->Init->getIO()
+        ->print_info( "Listening on "
+            . $self->Device
+            . " with "
+            . $self->Filter
+            . " Promisc:"
+            . $self->Promiscuous );
 
-	method run(){
-		my $d=$self->Init->getModuleLoader->loadmodule("Dispatcher");
-		$self->Dispatcher($d);
-		$self->Init->getIO()->print_info("Listening on ".$self->Device. " with ".$self->Filter." Promisc:".$self->Promiscuous);
-		
-		my $Dump = Net::Frame::Dump::Online->new(
-		   dev            => $self->Device,
-		   timeoutOnNext  => 3,
-		   timeout        => 0,
-		   promisc        => $self->Promiscuous,
-		   unlinkOnStop   => 1,
-		   file           => $self->File,
-		   filter         => $self->Filter,
-		   overwrite      => 0,
-		   isRunning      => 0,
-		   keepTimestamp  => 0,
-		   onRecvCount    => -1,
-		   frames         => [],
-		);
-		$self->SnifferInstance($Dump);
+    my $Dump = Net::Frame::Dump::Online->new(
+        dev           => $self->Device,
+        timeoutOnNext => 3,
+        timeout       => 0,
+        promisc       => $self->Promiscuous,
+        unlinkOnStop  => 1,
+        file          => $self->File,
+        filter        => $self->Filter,
+        overwrite     => 0,
+        isRunning     => 0,
+        keepTimestamp => 0,
+        onRecvCount   => -1,
+        frames        => [],
+    );
+    $self->SnifferInstance($Dump);
 
-		$Dump->start;
- 
+    $Dump->start;
 
-		while (1) {
-		   if (my $f = $Dump->next) {
-		      # my $raw            = $f->{raw};
-		      # my $firstLayerType = $f->{firstLayer};
-		      # my $timestamp      = $f->{timestamp};
-       		  my $oSimple = Net::Frame::Simple->newFromDump($f);
-       		#  $Init->io->debug($oSimple->print);
-		      $d->dispatch_packet($oSimple);
-		   }
-		}
+    while (1) {
+        if ( my $f = $Dump->next ) {
 
-		$Init->getIO()->print_alert("Exited from loop, something happened");
-	}
+            # my $raw            = $f->{raw};
+            # my $firstLayerType = $f->{firstLayer};
+            # my $timestamp      = $f->{timestamp};
+            my $oSimple = Net::Frame::Simple->newFromDump($f);
 
-	method stop(){
-		$self->SnifferInstance->stop();
-	}
+            #  $Init->io->debug($oSimple->print);
+            $d->dispatch_packet($oSimple);
+        }
+    }
 
-		
-
+    $Init->getIO()->print_alert("Exited from loop, something happened");
 }
+
+sub stop() {
+    my $self = shift;
+    $self->SnifferInstance->stop();
+}
+
 1;
