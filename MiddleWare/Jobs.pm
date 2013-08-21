90f6786eb205067b8dc4bf85d63bec6aa2c202cc
mudler <mudler@dark-lab.net>
2013-08-21 21:58:27 +0200

    code cleanup

--- MiddleWare/Jobs.pm
+++ MiddleWare/Jobs.pm
@@ -8,70 +8,67 @@ our $MODULE  = "Metasploit Module";
 our $INFO    = "<www.dark-lab.net>";
 
 #Funzioni che fornisco.
-our @PUBLIC_FUNCTIONS =
-    qw(list kill detach result status test clear);    #NECESSARY
-
+our @PUBLIC_FUNCTIONS
+    = qw(list kill detach result status test clear);    #NECESSARY
 
 nemesis module { $Init->io->info("test passed :)"); $self->import_jobs(); }
-has 'Processes' => (is=>"rw", default=>sub{ return [] } );
+has 'Processes' => ( is => "rw", default => sub { return [] } );
 
-sub test{
-	$Init->io->info("testing myself");
+sub test {
+    $Init->io->info("testing myself");
 }
 
-sub add{ #Not avaible from cli, but avaible among Plugins/MiddleWare
-	my $self=shift;
-	push(@{$self->Processes},$_);
-	return $self;
-}
-sub clear(){
-	my $self=shift;
-	$Init->io->info("cleaning not running and pending jobs");
-		    	foreach my $Proc(@{$self->Processes}){
-
-		    		$Proc->destroy() if(!$Proc->is_running);
-
-		    	}
-
-
+sub add {    #Not avaible from cli, but avaible among Plugins/MiddleWare
+    my $self = shift;
+    push( @{ $self->Processes }, $_ );
+    return $self;
 }
 
-sub list{
-	my $self=shift;
-	$Init->io->print_title("what are your modules?");
-	foreach my $Job (@{$self->Processes}){
-		$Init->io->process_status($Job);
-	}
-  }
+sub clear() {
+    my $self = shift;
+    $Init->io->info("cleaning not running and pending jobs");
+    foreach my $Proc ( @{ $self->Processes } ) {
 
-sub import_jobs(){
-	my $self=shift;
-	  opendir (DIR, $Init->getEnv()->tmp_dir() ) or $Init->io->error($!);
-	    while (my $file = readdir(DIR)) {	
-	    	next if $file eq ".." or $file eq ".";
-	    	next if $file !~ /\.lock/;
-	    	$file=~s/\..*$//g;
-	    	$Init->io->debug("checking $file");
-	    	my $found=0;
-	    	foreach my $Proc(@{$self->Processes}){
-	    		if($Proc->get_id eq $file){
-	    			my $Process=$Init->ml->loadmodule("Process");
-	    			$Process->load($file);
-	    			push(@{$self->Processes},$Process);
-	    			$found=1;
-	    			last;
-	    		}
-			}
-			if($found==0){
-				my $Process=$Init->ml->loadmodule("Process");
-	    			$Process->load($file);
-	    			push(@{$self->Processes},$Process);
-			}
+        $Proc->destroy() if ( !$Proc->is_running );
 
-	    }
-	    closedir(DIR);
+    }
+
 }
 
+sub list {
+    my $self = shift;
+    $Init->io->print_title("what are your modules?");
+    foreach my $Job ( @{ $self->Processes } ) {
+        $Init->io->process_status($Job);
+    }
+}
 
+sub import_jobs() {
+    my $self = shift;
+    opendir( DIR, $Init->getEnv()->tmp_dir() ) or $Init->io->error($!);
+    while ( my $file = readdir(DIR) ) {
+        next if $file eq ".." or $file eq ".";
+        next if $file !~ /\.lock/;
+        $file =~ s/\..*$//g;
+        $Init->io->debug("checking $file");
+        my $found = 0;
+        foreach my $Proc ( @{ $self->Processes } ) {
+            if ( $Proc->get_id eq $file ) {
+                my $Process = $Init->ml->loadmodule("Process");
+                $Process->load($file);
+                push( @{ $self->Processes }, $Process );
+                $found = 1;
+                last;
+            }
+        }
+        if ( $found == 0 ) {
+            my $Process = $Init->ml->loadmodule("Process");
+            $Process->load($file);
+            push( @{ $self->Processes }, $Process );
+        }
+
+    }
+    closedir(DIR);
+}
 
 1;
