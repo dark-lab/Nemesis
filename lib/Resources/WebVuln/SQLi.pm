1ea12fd2f14dec4e5b88cb4f46522a0159061166
mudler <mudler@dark-lab.net>
2014-05-02 00:50:31 +0200

    [enhancement] Basic SQLi tests

--- lib/Resources/WebVuln/SQLi.pm
+++ lib/Resources/WebVuln/SQLi.pm
@@ -2,30 +2,37 @@ package Resources::WebVuln::LFI;
 use Nemesis::BaseRes -base;
 use Resources::Network::HTTPInterface;
 
-has 'Crawler';
 has 'Bug';
-has 'Test'      => sub {"/proc/self/environ"};
-has 'TestRegex' => sub {"DOCUMENT_ROOT=\/|HTTP_USER_AGENT"};
 
-
-sub test() {
+sub test {
     my $self = shift;
-    my @URLS = @{ $self->Crawler->stripLinks };
+    my @URLS = @_;
+    my %Res;
     foreach my $url (@URLS) {
-        $self->Init->getIO()->print_info("Trying against $url");
-        my $Test
-            = "http://"
-            . $url
-            . $self->Bug
-            . "../../../../../../../../../../../../../"
-            . $self->Test . "%0000";
-        my $response=Resources::Network::HTTPInterface->new->get($Test);
-        my $Content = $response->{content};
-        my $r       = $self->TestRegex;
-        if ( $Content =~ /$r/i ) {
-            $self->Init->getIO()->print_info( $Test . " is vulnerable" );
+        $self->Init->getIO()->print_info("SQLinjection testing against $url");
+        my $Test     = "http://" . $url . $self->Bug . "'";
+        my $response = Resources::Network::HTTPInterface->new->get($Test);
+        my $Content  = $response->{content};
+        if ( $Content
+            =~ m/You have an error in your SQL syntax|Query failed|SQL query failed/i
+            )
+        {
+            $Res{$Test} = "mysql";
         }
+        elsif ( $Content
+            =~ m/ODBC SQL Server Driver|Unclosed quotation mark|Microsoft OLE DB Provider for/i
+            )
+        {
+            $Res{$Test} = "msserver";
+        }
+        elsif ( $Content
+            =~ m/Microsoft JET Database|ODBC Microsoft Access Driver/i )
+        {
+            $Res{$Test} = "access";
+        }
+
     }
+    return %Res;
 }
 
 1;
